12e867dc80a6fc127d5c07a37ca5cf5cbe81f73c
darshanrm <darshan.marathe13@gmail.com>
2021-11-02 17:10:39 +0530

    added oAuth login functionality

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
+require("./sevices/passport");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ new: "change" });
-});
+require("./routes/authRoutes")(app);
 
+//Check environment variables to see the port number which is dynamically configured by Heroku in env variables
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
